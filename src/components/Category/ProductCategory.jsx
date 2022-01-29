6776302f5d6c3a2f740634ab5f5649609aa0c597
윤창현 <changh950@gmail.com>
2022-01-29 10:35:33 +0900

    💄 카테고리 레이아웃 및 UI 스타일링 완료

---
+++ src/components/Category/ProductCategory.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+import category from 'data/category';
+import { COLOR } from 'constants';
+import { Cross } from '@styled-icons/entypo/Cross';
+import styled from 'styled-components';
+
+const ProductCategory = () => {
+  const [checkedList, setCheckedList] = useState([]);
+  const [showList, setShowList] = useState([]);
+
+  const checkedCategory = id => {
+    if (checkedList.indexOf(id) < 0) {
+      setCheckedList(checkedList => [...checkedList, id]);
+    } else {
+      setCheckedList(checkedList.filter(checkedList => checkedList !== id));
+    }
+  };
+
+  const showLists = id => {
+    if (showList.indexOf(id) === checkedList.indexOf(id)) {
+      category.map(item => (
+        <SelectBox key={item.id} value={item.id}>
+          {item.name} <CrossIcon onClick={() => deleteList(item.id)} />
+        </SelectBox>
+      ));
+    } else {
+      <ShowCategory>카테고리를 지정해 주세요</ShowCategory>;
+    }
+  };
+
+  const deleteList = id => {
+    setShowList(showList.filter(showList => showList !== id));
+  };
+
+  const ChoiceCategory = category.map(item => (
+    <Check key={item.id}>
+      <Input
+        type="checkbox"
+        value={item.id}
+        onClick={() => checkedCategory(item.id)}
+      />
+      <Label>{item.name}</Label>
+    </Check>
+  ));
+
+  const upRoadCategory = showLists() && (
+    <ShowCategory>카테고리를 지정해 주세요</ShowCategory>
+  );
+
+  return (
+    <CategorySection>
+      {/* {checkedList}
+      {showList} */}
+      <CategoryList>{ChoiceCategory}</CategoryList>
+      <SelectCategory>{upRoadCategory}</SelectCategory>
+    </CategorySection>
+  );
+};
+
+export default ProductCategory;
+
+const Input = styled.input.attrs({ type: 'checkBox' })`
+  -moz-appearance: none;
+  margin-left: 16px;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  :checked {
+    -webkit-appearance: none;
+    margin-left: 16px;
+    width: 20px;
+    height: 20px;
+    background: ${COLOR.MAIN};
+    border: 1px solid ${COLOR.BG};
+    border-radius: 5px;
+  }
+`;
+
+const Check = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  margin-left: 15px;
+
+  font-size: 20px;
+`;
+
+const ShowCategory = styled.div`
+  color: gray;
+`;
+
+const CrossIcon = styled(Cross)`
+  width: 20px;
+`;
+
+const SelectBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 15px;
+  width: 150px;
+  height: 40px;
+  border: 1px solid ${COLOR.MAIN};
+  border-radius: 3px;
+  background-color: ${COLOR.BG_LIGHT};
+`;
+
+const CategorySection = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CategoryList = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 10px 10px 10px 5px;
+  margin-right: 20px;
+  width: 400px;
+  height: 400px;
+  border: 1px solid ${COLOR.BG_LIGHT};
+  border-radius: 5px;
+`;
+
+const SelectCategory = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 10px;
+  width: 300px;
+  height: 400px;
+  border: 1px solid ${COLOR.BG_LIGHT};
+  border-radius: 5px;
+`;
--- src/pages/ch.jsx
+++ src/pages/ch.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { SectionBlock, OptionBlock } from 'components/Layouts';
 import ProductExposure from 'components/Period/ProductExposure';
 import ProductSale from 'components/Period/ProductSale';
-import styled from 'styled-components';
+import ProductCategory from 'components/Category/ProductCategory';
 
 const ProductPeriod = ({ name }) => {
   return (
-    <SectionBlock title="노출 및 판매기간 설정">
-      <OptionBlock name="상품 노출 기한">
-        <ProductExposure />
-      </OptionBlock>
-      <OptionBlock name="상품 판매 기한">
-        <ProductSale />
-      </OptionBlock>
-    </SectionBlock>
+    <>
+      <SectionBlock title="노출 및 판매기간 설정">
+        <OptionBlock name="상품 노출 기한">
+          <ProductExposure />
+        </OptionBlock>
+        <OptionBlock name="상품 판매 기한">
+          <ProductSale />
+        </OptionBlock>
+        <OptionBlock name="카테고리*">
+          <ProductCategory />
+        </OptionBlock>
+      </SectionBlock>
+    </>
   );
 };
 
 export default ProductPeriod;
-
-const Div = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
